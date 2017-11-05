fca7a4f66d0fddd5b232b869172c5443c8f542e2
sean5357 <shuan.wu@mail.utoronto.ca>
2017-11-05 11:20:53 -0500

    Added pages of ReturnUm Features for later deeper development.

---
+++ pages/ReturnUm/ReturnUm.js
@@ -0,0 +1,82 @@
+// ReturnUm.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  SendtoServer: function () {
+    var success = true;
+    //send
+    //get response from server
+    if (!success) {
+      wx.navigateTo({
+        url: '../ReturnResponseSuccess/Success'
+      })
+    } else {
+      wx.navigateTo({
+        url: '../ReturnResponseFail/Fail'
+      })
+    }
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/logs/logs.js
@@ -0,0 +1,23 @@
+//logs.js
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    logs: []
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(function (log) {
+        return util.formatTime(new Date(log))
+      })
+    })
+    console.log("page log onLoad");
+  }
+
+
+})
